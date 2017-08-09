a65fd284e1b98eb27256af61be0e8f24a558c428
dadafly133 <zhangdalin90@gmail.com>
2017-08-09 16:52:38 +1000

    modify comments

--- figure_gen.m
+++ figure_gen.m
@@ -2,43 +2,43 @@
 % train profile plot
 % export the figure as .eps format will keep the figure in high resolution in latex
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% file_name = "/home/dadafly/program/CNN_LSTM/result/conv_3l_win_10_108_fc_rnn2_fc_1024_N_075_train/conv_3l_win_10_108_rnn2_fc_1024_N.xlsx";
-% [data, header] = xlsread(file_name, 'result');
-% 
-% epochs = data(:,1);
-% test_accuracy = data(:,2);
-% test_loss = data(:,3);
-% train_accuracy = data(:,4);
-% train_loss = data(:,5);
-% 
-% figure;
-% hold on;
-% 
-% yyaxis left
-% plot(epochs(1:300), train_accuracy(1:300), 'DisplayName', 'train accuracy','LineWidth',3);
-% plot(epochs(1:300), test_accuracy(1:300), 'DisplayName', 'test accuracy','LineWidth',3);
-% 
-% set(gca,'FontSize',24,'FontWeight','bold','linewidth',2); % set axes style
-% 
-% xlabel('Epochs','FontSize',24,'FontWeight','bold');
-% ylabel('Accuracy','FontSize',24,'FontWeight','bold');
-% 
-% box on
-% grid on
-% % grid minor
-% 
-% yyaxis right
-% plot(epochs(1:300), train_loss(1:300), 'DisplayName', 'train loss','LineWidth',3);
-% plot(epochs(1:300), test_loss(1:300), 'DisplayName', 'test loss','LineWidth',3);
-% 
-% ylabel('Loss','FontSize',24,'FontWeight','bold');
-% lgd =legend('show');
-% lgd.Box = 'on';
-% %lgd.LineWidth = 1.5;
-% %lgd.FontSize = 18;
-% %lgd.FontWeight = 'normal';
-% 
-% hold off;
+file_name = "/home/dadafly/program/CNN_LSTM/result/conv_3l_win_10_108_fc_rnn2_fc_1024_N_075_train/conv_3l_win_10_108_rnn2_fc_1024_N.xlsx";
+[data, header] = xlsread(file_name, 'result');
+
+epochs = data(:,1);
+test_accuracy = data(:,2);
+test_loss = data(:,3);
+train_accuracy = data(:,4);
+train_loss = data(:,5);
+
+figure;
+hold on;
+
+yyaxis left
+plot(epochs(1:300), train_accuracy(1:300), 'DisplayName', 'train accuracy','LineWidth',3);
+plot(epochs(1:300), test_accuracy(1:300), 'DisplayName', 'test accuracy','LineWidth',3);
+
+set(gca,'FontSize',24,'FontWeight','bold','linewidth',2); % set axes style
+
+xlabel('Epochs','FontSize',24,'FontWeight','bold');
+ylabel('Accuracy','FontSize',24,'FontWeight','bold');
+
+box on
+grid on
+% grid minor
+
+yyaxis right
+plot(epochs(1:300), train_loss(1:300), 'DisplayName', 'train loss','LineWidth',3);
+plot(epochs(1:300), test_loss(1:300), 'DisplayName', 'test loss','LineWidth',3);
+
+ylabel('Loss','FontSize',24,'FontWeight','bold');
+lgd =legend('show');
+lgd.Box = 'on';
+%lgd.LineWidth = 1.5;
+%lgd.FontSize = 18;
+%lgd.FontWeight = 'normal';
+
+hold off;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -46,46 +46,46 @@
 % export the figure as .eps format will keep the figure in high resolution in latex
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% file_name = "/home/dadafly/program/CNN_LSTM/result/train_proportion_acc_.xlsx";
-% [data, header] = xlsread(file_name);
-% 
-% train_proportion 	= data(:,1);
-% test_accuracy 		= data(:,2);
-% train_time 			= data(:,8);
-% 
-% figure;
-% hold on;
-% 
-% yyaxis right 
-% h = plot(train_proportion, test_accuracy ,'-d', 'DisplayName', 'validation accuracy','LineWidth',3);
-% 
-% % set with plot display in front or backgroud
-% ax = gca;
-% ax.SortMethod = 'depth';
-% %********************************************
-% 
-% set(gca, 'FontSize',24,'FontWeight','bold','linewidth',2); % set axes style
-%  
-% xlabel('Train Proportion (%)','FontSize',24,'FontWeight','bold');
-% ylabel('Validation Accuracy','FontSize',24,'FontWeight','bold');
-%  
-% box on
-% % grid on
-% % grid minor
-% 
-% yyaxis left 
-% b = bar(train_proportion, train_time, 'DisplayName', 'train time');
-% b.FaceColor = [0 0.447 0.7];
-% b.EdgeColor = 'none';
-% 
-% ylabel('Train Time (s)','FontSize',24,'FontWeight','bold');
-% lgd =legend('show');
-% lgd.Box = 'on';
-% % lgd.LineWidth = 1.5;
-% % lgd.FontSize = 18;
-% % lgd.FontWeight = 'normal';
-% 
-% hold off;
+file_name = "/home/dadafly/program/CNN_LSTM/result/train_proportion_acc_.xlsx";
+[data, header] = xlsread(file_name);
+
+train_proportion 	= data(:,1);
+test_accuracy 		= data(:,2);
+train_time 			= data(:,8);
+
+figure;
+hold on;
+
+yyaxis right 
+h = plot(train_proportion, test_accuracy ,'-d', 'DisplayName', 'validation accuracy','LineWidth',3);
+
+% set with plot display in front or backgroud
+ax = gca;
+ax.SortMethod = 'depth';
+%********************************************
+
+set(gca, 'FontSize',24,'FontWeight','bold','linewidth',2); % set axes style
+ 
+xlabel('Train Proportion (%)','FontSize',24,'FontWeight','bold');
+ylabel('Validation Accuracy','FontSize',24,'FontWeight','bold');
+ 
+box on
+% grid on
+% grid minor
+
+yyaxis left 
+b = bar(train_proportion, train_time, 'DisplayName', 'train time');
+b.FaceColor = [0 0.447 0.7];
+b.EdgeColor = 'none';
+
+ylabel('Train Time (s)','FontSize',24,'FontWeight','bold');
+lgd =legend('show');
+lgd.Box = 'on';
+% lgd.LineWidth = 1.5;
+% lgd.FontSize = 18;
+% lgd.FontWeight = 'normal';
+
+hold off;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
